198b1b1b228458feeaaf729f3f4b8b2a2e0beed6
vikramkumartejani <vikramkumar8t8@gmail.com>
2023-11-07 05:38:42 +0000

    'update'

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-let currentSlide = 0;
-  const slides = document.querySelectorAll('.slide');
-  const totalSlides = slides.length;
-
-  function nextSlide() {
-    slides[currentSlide].style.transform = 'translateX(-100%)';
-    currentSlide = (currentSlide + 1) % totalSlides;
-    slides[currentSlide].style.transform = 'translateX(0)';
-  }
-
-  setInterval(nextSlide, 2000); // Change slide every 2 seconds
+function toggleMenu() {
+  var menu = document.querySelector('.menu');
+  menu.classList.toggle('active');
+}

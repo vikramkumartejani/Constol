058683128431b0d653901fe3de14aa826cddd0c6
vikramkumartejani <vikramkumar8t8@gmail.com>
2023-11-07 06:06:26 +0000

    'update'

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   var menu = document.querySelector('.menu');
   menu.classList.toggle('active');
 }
+
+// top navbar
+function toggleHeader() {
+  var headerTop = document.getElementById('headerTop');
+  if (headerTop.style.display === 'none') {
+      headerTop.style.display = 'block'; // If hidden, show the header
+  } else {
+      headerTop.style.display = 'none'; // If visible, hide the header
+  }
+}
